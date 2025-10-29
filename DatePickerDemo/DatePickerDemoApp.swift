25b9c2f69b74e3ce0abb0a69b224e903806b7177
Irina Saf <irina.safronova0801@gmail.com>
2025-10-29 10:16:10 -0400

    Initial Commit

---
+++ DatePickerDemo/DatePickerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickerDemoApp.swift
+//  DatePickerDemo
+//
+//  Created by Irina Saf on 2025-10-29.
+//
+
+import SwiftUI
+
+@main
+struct DatePickerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
